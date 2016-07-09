3034a4de6868fd4f27abec7c840202c2307b37c5
chocko <malo_s8s1@hotmail.com>
2016-07-09 11:41:34 -0500

    Ajax remoto  v20

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,8 +10,8 @@ class CommentsController < ApplicationController
     @comment.article= @article
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @article, notice: 'Comment was successfully created.' }
-        format.json { render :show, status: :created, location: @comment }
+        #format.html { redirect_to @article, notice: 'Comment was successfully created.' }
+        format.json { render :show, status: :created, location: @comment.article }
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
